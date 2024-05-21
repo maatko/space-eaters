d3388eda177791f5a14e252d23060486afce526e
maatko <matkovukovic25@gmail.com>
2024-05-21 21:12:14 +0200

    cleanup

--- src/sprite/spritesheet.c
+++ src/sprite/spritesheet.c
@@ -2,18 +2,18 @@
 
 spritesheet_t spritesheet = {
     .sprites = {
-        .logo           = { 73, 74, 47, 23 },
-        .frame_left     = { 1, 106, 31, 22 },
+        .logo           = { 73, 74, 47, 23  },
+        .frame_left     = { 1, 106, 31, 22  },
         .frame_full     = { 32, 106, 64, 22 },
         .frame_right    = { 96, 106, 31, 22 },
-        .player_left    = { 9, 1, 7, 7 },
-        .player_still   = { 0, 1, 8, 7 },
-        .player_right   = { 0, 34, 8, 7 },
-        .boss           = { 41, 74, 30, 23 },
-        .heart          = { 51, 20, 8, 7 },
-        .bomb           = { 121, 9, 6, 7 },
-        .bullet         = { 19, 52, 2, 3 },
-        .star           = { 2, 88, 3, 3 }
+        .player_left    = { 9, 1, 7, 7      },
+        .player_still   = { 0, 1, 8, 7      },
+        .player_right   = { 0, 34, 8, 7     },
+        .boss           = { 41, 74, 30, 23  },
+        .heart          = { 51, 20, 8, 7    },
+        .bomb           = { 121, 9, 6, 7    },
+        .bullet         = { 19, 52, 2, 3    },
+        .star           = { 2, 88, 3, 3     }
     }
 };
 
